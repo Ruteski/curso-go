b2bf21bcdef24105c476b5771b1c402a6001e3b2
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 19:55:16 -0300

    type assertation

---
+++ 1-fundamentos/18-type-assertation/main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var minhaVar interface{} = "Lincoln Ruteski"
+	println(minhaVar)
+	println(minhaVar.(string)) // isso é o type assertation, confirmar que a variavel dé de um certo tipo
+
+	res, ok := minhaVar.(int)
+	fmt.Printf("O valor de res é %v e o resultado de ok é %v \n", res, ok)
+
+	res2 := minhaVar.(int)
+	fmt.Printf("O valor de res2 é %v", res2)
+
+}
