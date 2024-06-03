bf5073edd8789dc91e5f31e6dd417ffc0db1d132
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 14:17:59 -0300

    funcoes variaticas

---
+++ 1-fundamentos/08-funcoes-variaticas/main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Primeiro sum: ", sum(1, 3))
+	fmt.Println("Segundo sum: ", sum(1, 3, 2, 6, 8, 9, 34, 5, 335, 78, 6543, 4, 6, 76, 67, 2))
+}
+
+func sum(numeros ...int) int {
+	total := 0
+
+	for _, numero := range numeros {
+		total += numero
+	}
+
+	return total
+}
