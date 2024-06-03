bf5a5da538aa6a85640f1f5fbda1975f7aba8a5e
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 15:19:16 -0300

    ponteiros

---
+++ 1-fundamentos/14-ponteiros/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+func main() {
+	// Memoria —> Endrego —> Valor
+	// variavel -> ponteiro que tem um endereco na memora —> valor
+	a := 10
+	println(a)
+	println(&a) // printa o endereco de memoria de 'a'
+
+	var ponteiro *int = &a
+	println(ponteiro)
+	*ponteiro = 20
+	println(*ponteiro)
+	println("Novo valor de 'a': ", a)
+
+	b := &a
+	println(b)
+	println(*b) // *b chamado de direfence
+	*b = 30
+
+	println("valor de a:", a)
+	println("valor de a na memoria:", &a)
+	println("valor de b:", b)
+
+}
