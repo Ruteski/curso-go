f29637ce610cd9081727af60b12f71c048680e65
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 16:47:06 -0300

    interfaces vcazias(parecido com tipo generico)

---
+++ 1-fundamentos/17-interfaces-vazias/main.go
@@ -0,0 +1,16 @@
+﻿package main
+
+import "fmt"
+
+// parecido com tipo generico <T>
+func main() {
+	var x interface{} = 10
+	var y interface{} = "hello world"
+
+	showType(x)
+	showType(y)
+}
+
+func showType(t interface{}) {
+	fmt.Printf("O tipo da variavel é %T e o valor é %v \n", t, t)
+}
