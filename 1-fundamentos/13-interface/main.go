b3d50cf6e5464a7234841d4d09d521fcf634ad3e
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 15:05:38 -0300

    structs interface

---
+++ 1-fundamentos/13-interface/main.go
@@ -0,0 +1,58 @@
+﻿package main
+
+import "fmt"
+
+// a interface só me permite passar metodos
+type Pessoa interface {
+	Desativar()
+}
+
+type Endereco struct {
+	Rua    string
+	Numero int
+	Cidade string
+	Estado string
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+type Empresa struct {
+	Nome string
+}
+
+func (e Empresa) Desativar() {
+
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado \n", c.Nome)
+}
+
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
+
+func main() {
+	lincoln := Cliente{
+		Nome:  "Lincoln",
+		Idade: 40,
+		Ativo: true,
+		Endereco: Endereco{
+			Rua:    "antonio",
+			Numero: 10,
+			Cidade: "Curitiba",
+			Estado: "Parana",
+		},
+	}
+
+	minhaEmpresa := Empresa{}
+
+	Desativacao(lincoln)
+	Desativacao(minhaEmpresa)
+}
