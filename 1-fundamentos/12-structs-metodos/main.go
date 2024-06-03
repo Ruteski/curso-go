59fbcb9fd9b10ec93bb2601f53c14a26f3dd3f66
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 14:51:44 -0300

    structs metodos

---
+++ 1-fundamentos/12-structs-metodos/main.go
@@ -0,0 +1,40 @@
+﻿package main
+
+import "fmt"
+
+type Endereco struct {
+	Rua    string
+	Numero int
+	Cidade string
+	Estado string
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado \n", c.Nome)
+}
+
+func main() {
+	lincoln := Cliente{
+		Nome:  "Lincoln",
+		Idade: 40,
+		Ativo: true,
+		Endereco: Endereco{
+			Rua:    "antonio",
+			Numero: 10,
+			Cidade: "Curitiba",
+			Estado: "Parana",
+		},
+	}
+
+	fmt.Println(lincoln, "\n")
+	lincoln.Desativar()
+
+}
