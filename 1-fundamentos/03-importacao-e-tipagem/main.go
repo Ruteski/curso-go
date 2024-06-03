76e00f05429e4e3bcd11d38c1f7f59a17c68f00b
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 12:34:57 -0300

    importacao e tipagem

---
+++ 1-fundamentos/03-importacao-e-tipagem/main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import "fmt"
+
+type ID int
+
+var e = 1.2
+
+func main() {
+	var a ID = 20
+
+	// %T - traz o tipo da variavel
+	fmt.Printf("O tipo de 'E' é %T", e)
+	fmt.Println("")
+	fmt.Printf("O tipo de 'E' é %v", e)
+	fmt.Println("")
+	fmt.Printf("O tipo de 'A' é %T", a)
+}
