7db3934dc47ac0c89cb9bea9dd82ad4ba39a1120
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 14:33:30 -0300

    struct inicio

---
+++ 1-fundamentos/10-structs-inicio/main.go
@@ -0,0 +1,21 @@
+﻿package main
+
+import "fmt"
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+}
+
+func main() {
+	lincoln := Cliente{
+		Nome:  "Lincoln",
+		Idade: 40,
+		Ativo: true,
+	}
+	fmt.Printf("Nome: %s \n Idade: %d\n Ativo: %t\n", lincoln.Nome, lincoln.Idade, lincoln.Ativo)
+
+	lincoln.Ativo = false
+	fmt.Printf("Nome: %s \n Idade: %d\n Ativo: %t\n", lincoln.Nome, lincoln.Idade, lincoln.Ativo)
+}
