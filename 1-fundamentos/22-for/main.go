c31e7af737157d8c6f61b7a99455244797e10f88
Lincoln Ruteski <lruteski@gmail.com>
2024-06-03 21:57:36 -0300

    laço for

---
+++ 1-fundamentos/22-for/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+func main() {
+	//for i := 0; i < 10; i++ {
+	//	print(i)
+	//}
+
+	//numeros := []string{"um", "dois", "tres"}
+	//for k, v := range numeros {
+	//	println(k, v)
+	//}
+
+	// isso seria o while em outras linguagens
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	// sim loop infinito, suponha que precisamos ficar escutando uma chamada ou esperando receber algo
+	// for {
+	// 	println("hello world")
+	// }
+
+}
