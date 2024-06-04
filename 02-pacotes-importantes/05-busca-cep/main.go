95fb8a41bde741fa22335f21d56d99df200567a1
Lincoln Ruteski <lruteski@gmail.com>
2024-06-04 20:50:15 -0300

    buscar cep

---
+++ 02-pacotes-importantes/05-busca-cep/main.go
@@ -0,0 +1,60 @@
+﻿package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	// pega letra a letra que digitei no terminal e retorna um slice
+	//for _, url := range os.Args[1:] {
+	for _, cep := range os.Args[1:] {
+		//req, err := http.Get(url)
+		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer requisicao: %v\n", err)
+		}
+		defer req.Body.Close()
+
+		//=== lendo a resposta do get
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler a resposta: %v\n", err)
+		}
+
+		//=== transformando o json em struct
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v\n", err)
+		}
+		//fmt.Println(data.Localidade)
+
+		//=== gravando o retorno em uma arquivo
+		file, err := os.Create("cidade.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
+		fmt.Println("Arquivo criado com sucesso!")
+		fmt.Println("Cidade: ", data.Localidade)
+	}
+}
