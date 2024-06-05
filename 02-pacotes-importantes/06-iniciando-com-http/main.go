e9caebe8efdc236d27995acadd1b8049f8f09a1d
Lincoln Ruteski <lruteski@gmail.com>
2024-06-04 21:01:12 -0300

    iniciando com o http

---
+++ 02-pacotes-importantes/06-iniciando-com-http/main.go
@@ -0,0 +1,17 @@
+﻿package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/new", func(w http.ResponseWriter, request *http.Request) {
+		w.Write([]byte("Hello World 2, com func anonima"))
+	})
+	http.ListenAndServe(":8080", nil) //isso cria um http server
+}
+
+func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
