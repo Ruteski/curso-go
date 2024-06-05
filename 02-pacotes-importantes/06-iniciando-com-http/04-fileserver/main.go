09f67f422c8dd431945fdfc672068b15fb022612
Lincoln Ruteski <lruteski@gmail.com>
2024-06-05 15:51:34 -0300

    fileserver

---
+++ 02-pacotes-importantes/06-iniciando-com-http/04-fileserver/main.go
@@ -0,0 +1,17 @@
+﻿package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./public"))
+	mux := http.NewServeMux()
+	mux.Handle("/", fileserver)
+	mux.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hellow Blog"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
