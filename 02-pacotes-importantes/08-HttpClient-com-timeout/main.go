eb78d5d1813907516a4332792e726c4b7160379c
Lincoln Ruteski <lruteski@gmail.com>
2024-06-05 21:31:45 -0300

    HttpClient com timeout

---
+++ 02-pacotes-importantes/08-HttpClient-com-timeout/main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// c := http.Client{Timeout: time.Second}
+	// c := http.Client{Timeout: 5 * time.Second}
+	// c := http.Client{Timeout: time.Duration(1) * time.Second}
+	c := http.Client{Timeout: time.Microsecond}
+	resp, err := c.Get("http://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+
+}
