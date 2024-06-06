71a13e5473a66b662ba2eb30875028f98d5b6d1b
Lincoln Ruteski <lruteski@gmail.com>
2024-06-06 15:24:40 -0300

    context: key value

---
+++ 03-context/2-utilizando-server-http/with-key-value/main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "token", "senha123")
+	bookHotel(ctx, "Hotel")
+}
+
+// sempre que usar o context como param, passar por primeiro
+func bookHotel(ctx context.Context, name string) {
+	token := ctx.Value("token")
+	fmt.Println(token)
+}
