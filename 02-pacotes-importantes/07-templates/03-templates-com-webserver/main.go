d4df8c6efaee534d20268084b35c0df269e331ad
Lincoln Ruteski <lruteski@gmail.com>
2024-06-05 21:07:32 -0300

    templates - com webserver

---
+++ 02-pacotes-importantes/07-templates/03-templates-com-webserver/main.go
@@ -0,0 +1,31 @@
+﻿package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.New("template.html").ParseFiles("template.html"))
+		err := t.Execute(w, Cursos{
+			{"Go", 40},
+			{"Java", 20},
+			{"Python", 10},
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	println("Server running on port 8282🚀🚀🚀")
+	http.ListenAndServe(":8282", nil)
+
+}
