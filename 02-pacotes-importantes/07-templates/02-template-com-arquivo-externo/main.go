e08f432287102af7cfcee199a430fb5000479a9c
Lincoln Ruteski <lruteski@gmail.com>
2024-06-06 14:49:27 -0300

    context: introducao

--- 02-pacotes-importantes/07-templates/02-template-com-arquivo-externo/main.go
+++ 02-pacotes-importantes/07-templates/02-template-com-arquivo-externo/main.go
@@ -13,7 +13,7 @@ type Curso struct {
 type Cursos []Curso
 
 func main() {
-	t := template.Must(template.New("template.html").ParseFiles("template.html"))
+	t := template.Must(template.New("content.html").ParseFiles("content.html"))
 	err := t.Execute(os.Stdout, Cursos{
 		{"Go", 40},
 		{"Java", 20},
---
+++ 03-context/1-hotel/main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	//ctx, cancel := context.WithTimeout(ctx, time.Second*6)
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+
+	bookHotel(ctx)
+}
+
+func bookHotel(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("Hotel booking cancelled. Timeout reached")
+		return
+	case <-time.After(5 * time.Second):
+		fmt.Println("Hotel booking completed")
+
+	}
+}
