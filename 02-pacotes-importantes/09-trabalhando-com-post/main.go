2275379721e72ebbbe077455c25c38a66c4670eb
Lincoln Ruteski <lruteski@gmail.com>
2024-06-05 21:38:16 -0300

    trabalhando com post

---
+++ 02-pacotes-importantes/09-trabalhando-com-post/main.go
@@ -0,0 +1,22 @@
+﻿package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	c := http.Client{}
+	jsonVar := bytes.NewBuffer([]byte(`{"name": "lincoln"}`))
+
+	resp, err := c.Post("http://www.google.com", "application/json", jsonVar)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
+
+}
