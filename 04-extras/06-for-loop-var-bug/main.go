2f1a8c747dd6f71a56ec43ed50020e93c843571e
Lincoln Ruteski <lruteski@gmail.com>
2024-06-15 11:23:23 -0300

    extra: for loop var

---
+++ 04-extras/06-for-loop-var-bug/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	for i := range x {
+		fmt.Println(i)
+	}
+
+	done := make(chan bool)
+	values := []string{"a", "b", "c"}
+
+	for _, v := range values {
+		// v := v // assim que arrumavam antes da versao 1.22
+		go func() {
+			fmt.Println(v)
+			done <- true
+		}()
+	}
+
+	for range values {
+		<-done
+	}
+}
