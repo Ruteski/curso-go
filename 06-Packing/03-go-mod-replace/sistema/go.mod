c32bdb340359418538e51766e4a2a78b1974ab3a
Lincoln Ruteski <lruteski@gmail.com>
2024-06-26 15:21:23 -0300

    packaging: go mod replace
    gambiarra, hoje tem o workspaces

---
+++ 06-Packing/03-go-mod-replace/sistema/go.mod
@@ -0,0 +1,7 @@
+module curso-go/sistema
+
+go 1.22.4
+
+replace curso-go/math => ../math
+
+require curso-go/math v0.0.0-00010101000000-000000000000
---
+++ 06-Packing/03-go-mod-replace/sistema/main.go
@@ -0,0 +1,13 @@
+﻿package main
+
+// GAMBIARRA - rs
+// quando no pacote nao estiver publicado e só estiver na maquina, pode ser usado dessa forma, mas nao mto recomendado, tem o workspaces
+// 1 - go mod edit -replace curso-go/math=../math
+// 2 - go mod tidy
+
+import "curso-go/math"
+
+func main() {
+	m := math.NewMath(1, 2)
+	println(m.Add())
+}
