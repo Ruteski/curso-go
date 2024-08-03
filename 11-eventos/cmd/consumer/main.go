dedc1b89afac5b1235424fcb9f7455f30b621d71
Lincoln Ruteski <lruteski@gmail.com>
2024-08-03 16:04:57 -0300

    Eventos: RabbitMQ - criado o consumidor na pratica

---
+++ 11-eventos/cmd/consumer/main.go
@@ -0,0 +1,23 @@
+﻿package main
+
+import (
+	"eventos/pkg/rabbitmq"
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs)
+	for msg := range msgs {
+		fmt.Printf("Received a message: %s\n", msg.Body)
+		msg.Ack(false) // mensagem ja foi lida e nao é pra colocar ela na fila novamente
+	}
+}
