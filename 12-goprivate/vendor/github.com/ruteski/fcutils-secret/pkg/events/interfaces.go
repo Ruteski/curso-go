e5f9194522d76be7d97f6f7b54bef2f065db5352
Lincoln Ruteski <lruteski@gmail.com>
2024-12-08 12:55:24 -0300

    Goprivater
    go proxy X vendor(tipo nodemodules do go)

---
+++ 12-goprivate/vendor/github.com/ruteski/fcutils-secret/pkg/events/interfaces.go
@@ -0,0 +1,28 @@
+﻿package events
+
+import (
+	"sync"
+	"time"
+)
+
+type EventInterface interface {
+	GetName() string
+	GetDateTime() time.Time
+	GetPayload() interface{}
+}
+
+// EventHandlerInterface operações que serão executadas quando um evento é chamado
+type EventHandlerInterface interface {
+	Handle(event EventInterface, wg *sync.WaitGroup)
+}
+
+// EventDispatcherInterface gerenciador dos nossos eventos/operações
+// registra os eventos e suas operações
+// despachar/fire no evento para que suas operações sejam executadas
+type EventDispatcherInterface interface {
+	Register(eventName string, handler EventHandlerInterface) error
+	Dispatch(event EventInterface) error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear() error
+}
