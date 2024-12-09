c1aea123164b3b36a04570f42ee8dc7c6a8c6598
Lincoln Ruteski <lruteski@gmail.com>
2024-12-09 18:32:09 -0300

    gRPC: implementado o CreateCategory

---
+++ 14-gRPC/internal/service/category.go
@@ -0,0 +1,37 @@
+﻿package service
+
+import (
+	"14-gRPC/internal/database"
+	"14-gRPC/internal/pb"
+	"context"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	// return nil, status.Errorf(codes.Unimplemented, "method CreateCategory not implemented")
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		//return nil, status.Error(codes.Internal, err.Error())
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
