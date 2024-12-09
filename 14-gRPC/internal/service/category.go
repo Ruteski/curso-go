7c6e286635ba36fc1ca067f996b5deae978e6053
Lincoln Ruteski <lruteski@gmail.com>
2024-12-09 19:16:55 -0300

    gRPC: listando categories

--- 14-gRPC/internal/service/category.go
+++ 14-gRPC/internal/service/category.go
@@ -35,3 +35,21 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 		Category: categoryResponse,
 	}, nil
 }
+
+func (c *CategoryService) ListCategory(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+	for _, category := range categories {
+		categoriesResponse = append(categoriesResponse, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
