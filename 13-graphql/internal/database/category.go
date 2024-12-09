05836b1289613e947fd372a62c318f3572234e0f
Lincoln Ruteski <lruteski@gmail.com>
2024-12-09 09:46:30 -0300

    GraphQL: criado query para category

--- 13-graphql/internal/database/category.go
+++ 13-graphql/internal/database/category.go
@@ -28,3 +28,24 @@ func (c *Category) Create(name string, description string) (Category, error) {
 }
 
 //  6502assembly 6502asm
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := []Category{}
+
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+
+	return categories, nil
+}
