f249e159784be4b874020f882f4fd323e0220505
Lincoln Ruteski <lruteski@gmail.com>
2024-12-13 14:16:56 -0300

    S3: criado gerador de arquivos de exemplo

---
+++ 15-uploadS3/cmd/generator/main.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := range 1000 {
+		f, err := os.Create(fmt.Sprintf("./tmp/file%d.txt", i))
+
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		f.WriteString("Hello World!")
+		i++
+	}
+}
---
+++ 15-uploadS3/go.mod
@@ -0,0 +1,3 @@
+module 15-uploads3
+
+go 1.23.3
