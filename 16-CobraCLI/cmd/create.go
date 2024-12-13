7f7b41bcba906c000752c5596229ee09d9b59346
Lincoln Ruteski <lruteski@gmail.com>
2024-12-13 16:24:39 -0300

    CLI: flags locais x globais

--- 16-CobraCLI/cmd/create.go
+++ 16-CobraCLI/cmd/create.go
@@ -1,12 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -21,12 +18,13 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+		cmd.Help()
 	},
 }
 
 func init() {
 	categoryCmd.AddCommand(createCmd)
+	//	createCmd.PersistentFlags().String("name", "", "Nome da categoria")
 
 	// Here you will define your flags and configuration settings.
 
