0cf90a5e76078697bc820647fc1f61c86c535578
Lincoln Ruteski <lruteski@gmail.com>
2024-12-13 17:11:26 -0300

    CLI: entendendo os hooks

--- 16-CobraCLI/cmd/category.go
+++ 16-CobraCLI/cmd/category.go
@@ -30,6 +30,18 @@ to quickly create a Cobra application.`,
 		id, _ := cmd.Flags().GetInt16("id")
 		fmt.Println("ID da categoria é: ", id)
 	},
+
+	PreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Chamado antes do Run.")
+	},
+
+	PostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Chamado depois do Run.")
+	},
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("ocorreu um erro")
+	},
 }
 
 var category string
