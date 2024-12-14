4a76b9702eb1fee66cced6346314e287e442c38e
Lincoln Ruteski <lruteski@gmail.com>
2024-12-14 16:48:26 -0300

    DI: criado estrutura com dependencias, arvore de dependencia

--- 19-DI/go.mod
+++ 19-DI/go.mod
@@ -1,3 +1,5 @@
 module 19-DI
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24
---
+++ 19-DI/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import (
+	"19-DI/product"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	repository := product.NewProductRepository(db)
+	usecase := product.NewProductUseCase(repository)
+
+	product, err := usecase.GetProduct(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(product.Name)
+}
