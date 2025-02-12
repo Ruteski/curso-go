36affe37704f69deed845ec4dc08a864f0347691
Lincoln Ruteski <lruteski@gmail.com>
2025-02-12 15:37:01 -0300

    Labs - concorrencia: conectando projeto com o mongoDb

--- 22-labs/03-concorrencia-leilao/cmd/auction/main.go
+++ 22-labs/03-concorrencia-leilao/cmd/auction/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"auction_go/configuration/database/mongodb"
+	"context"
 	"log"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	ctx := context.Background()
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error trying to load .env file")
 		return
 	}
+
+	databaseClient, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
 }
