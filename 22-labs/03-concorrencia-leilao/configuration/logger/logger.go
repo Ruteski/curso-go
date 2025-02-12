51dbda6a0169e4d2cc3125f91db8505de2380604
Lincoln Ruteski <lruteski@gmail.com>
2025-02-12 15:08:59 -0300

    Labs - concorrencia: configurações iniciais

---
+++ 22-labs/03-concorrencia-leilao/configuration/logger/logger.go
@@ -0,0 +1,43 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log *zap.Logger
+)
+
+func init() {
+	logConfig := zap.Config{
+		Level:    zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding: "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:     "message",
+			TimeKey:        "time",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+	}
+
+	log, _ = logConfig.Build()
+}
+
+func Info(message string, tags ...zap.Field) {
+	log.Info(message, tags...)
+	log.Sync()
+}
+
+func Error(message string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Error(message, tags...)
+	log.Sync()
+}
