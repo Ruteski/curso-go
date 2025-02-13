dd931829efa8940cb3f7c0fb8162a3c053c91914
Lincoln Ruteski <lruteski@gmail.com>
2025-02-13 14:58:37 -0300

    Labs - concorrencia: criado usecase para regras de leilao

--- 22-labs/03-concorrencia-leilao/internal/entity/auction_entity/auction_entity.go
+++ 22-labs/03-concorrencia-leilao/internal/entity/auction_entity/auction_entity.go
@@ -1,40 +1,44 @@
 package auction_entity
 
 import (
+	"auction_go/internal/internal_error"
+	"context"
 	"time"
+
+	"github.com/google/uuid"
 )
 
-// func CreateAuction(
-// 	productName, category, description string,
-// 	condition ProductCondition) (*Auction, *internal_error.InternalError) {
-// 	auction := &Auction{
-// 		Id:          uuid.New().String(),
-// 		ProductName: productName,
-// 		Category:    category,
-// 		Description: description,
-// 		Condition:   condition,
-// 		Status:      Active,
-// 		Timestamp:   time.Now(),
-// 	}
+func CreateAuction(
+	productName, category, description string,
+	condition ProductCondition) (*Auction, *internal_error.InternalError) {
+	auction := &Auction{
+		Id:          uuid.New().String(),
+		ProductName: productName,
+		Category:    category,
+		Description: description,
+		Condition:   condition,
+		Status:      Active,
+		Timestamp:   time.Now(),
+	}
 
-// 	if err := auction.Validate(); err != nil {
-// 		return nil, err
-// 	}
+	if err := auction.Validate(); err != nil {
+		return nil, err
+	}
 
-// 	return auction, nil
-// }
+	return auction, nil
+}
 
-// func (au *Auction) Validate() *internal_error.InternalError {
-// 	if len(au.ProductName) <= 1 ||
-// 		len(au.Category) <= 2 ||
-// 		len(au.Description) <= 10 && (au.Condition != New &&
-// 			au.Condition != Refurbished &&
-// 			au.Condition != Used) {
-// 		return internal_error.NewBadRequestError("invalid auction object")
-// 	}
+func (au *Auction) Validate() *internal_error.InternalError {
+	if len(au.ProductName) <= 1 ||
+		len(au.Category) <= 2 ||
+		len(au.Description) <= 10 && (au.Condition != New &&
+			au.Condition != Refurbished &&
+			au.Condition != Used) {
+		return internal_error.NewBadRequestError("invalid auction object")
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
 type Auction struct {
 	Id          string
@@ -60,16 +64,16 @@ const (
 	Refurbished
 )
 
-// type AuctionRepositoryInterface interface {
-// 	CreateAuction(
-// 		ctx context.Context,
-// 		auctionEntity *Auction) *internal_error.InternalError
+type AuctionRepositoryInterface interface {
+	CreateAuction(
+		ctx context.Context,
+		auctionEntity *Auction) *internal_error.InternalError
 
-// 	FindAuctions(
-// 		ctx context.Context,
-// 		status AuctionStatus,
-// 		category, productName string) ([]Auction, *internal_error.InternalError)
+	FindAuctions(
+		ctx context.Context,
+		status AuctionStatus,
+		category, productName string) ([]Auction, *internal_error.InternalError)
 
-// 	FindAuctionById(
-// 		ctx context.Context, id string) (*Auction, *internal_error.InternalError)
-// }
+	FindAuctionById(
+		ctx context.Context, id string) (*Auction, *internal_error.InternalError)
+}
