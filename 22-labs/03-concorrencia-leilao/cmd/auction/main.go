164d1d29226f62c20c6f52281a26a33209d90872
Lincoln Ruteski <lruteski@gmail.com>
2025-02-13 19:19:40 -0300

    Labs - concorrencia: criado controller e rotas para lance

--- 22-labs/03-concorrencia-leilao/cmd/auction/main.go
+++ 22-labs/03-concorrencia-leilao/cmd/auction/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"auction_go/configuration/database/mongodb"
+	"auction_go/internal/infra/api/web/controller/auction_controller"
+	"auction_go/internal/infra/api/web/controller/bid_controller"
+	"auction_go/internal/infra/api/web/controller/user_controller"
+	"auction_go/internal/infra/database/auction"
+	"auction_go/internal/infra/database/bid"
+	"auction_go/internal/infra/database/user"
+	"auction_go/internal/usecase/auction_usecase"
+	"auction_go/internal/usecase/bid_usecase"
+	"auction_go/internal/usecase/user_usecase"
 	"context"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
@@ -15,10 +26,41 @@ func main() {
 		return
 	}
 
-	databaseClient, err := mongodb.NewMongoDBConnection(ctx)
+	databaseConnection, err := mongodb.NewMongoDBConnection(ctx)
 	if err != nil {
 		log.Fatal(err.Error())
 		return
 	}
 
+	router := gin.Default()
+
+	userController, bidController, auctionsController := initDependencies(databaseConnection)
+
+	router.GET("/auctions", auctionsController.FindAuctions)
+	router.GET("/auctions/:auctionId", auctionsController.FindAuctionById)
+	router.POST("/auctions", auctionsController.CreateAuction)
+	router.GET("/auction/winner/:auctionId", auctionsController.FindWinningBidByAuctionId)
+
+	router.POST("/bid", bidController.CreateBid)
+	router.GET("/bid/:auctionId", bidController.FindBidByAuctionId)
+
+	router.GET("/user/:userId", userController.FindUserById)
+
+	router.Run(":8080")
+}
+
+func initDependencies(database *mongo.Database) (
+	userController *user_controller.UserController,
+	bidController *bid_controller.BidController,
+	auctionController *auction_controller.AuctionController) {
+
+	auctionRepository := auction.NewAuctionRepository(database)
+	bidRepository := bid.NewBidRepository(database, auctionRepository)
+	userRepository := user.NewUserRepository(database)
+
+	userController = user_controller.NewUserController(user_usecase.NewUserUseCase(userRepository))
+	auctionController = auction_controller.NewAuctionController(auction_usecase.NewAuctionUseCase(auctionRepository, bidRepository))
+	bidController = bid_controller.NewBidController(bid_usecase.NewBidUseCase(bidRepository))
+
+	return
 }
