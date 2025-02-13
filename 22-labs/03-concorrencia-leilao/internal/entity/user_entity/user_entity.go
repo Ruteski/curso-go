64a28662e3f548bb9c7909edc04412272b07923b
Lincoln Ruteski <lruteski@gmail.com>
2025-02-13 14:42:03 -0300

    Labs - concorrencia: criado usecase para regras do usuario

--- 22-labs/03-concorrencia-leilao/internal/entity/user_entity/user_entity.go
+++ 22-labs/03-concorrencia-leilao/internal/entity/user_entity/user_entity.go
@@ -1,8 +1,17 @@
 package user_entity
 
+import (
+	"auction_go/internal/internal_error"
+	"context"
+)
+
 type User struct {
 	Id   string
 	Name string
 	//	Username string
 	//	Password string
+}
+
+type UserRepositoryInterface interface {
+	FindUserById(ctx context.Context, userId string) (*User, *internal_error.InternalError)
 }
